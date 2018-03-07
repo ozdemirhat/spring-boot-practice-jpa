cc8b6320bf42c2b058a3ec01f1e8ca5048d41a84
hatice.ozdemir <hatice.ozdemir@trendyol.com>
2018-03-07 17:19:31 +0300

    User class and its getters and setters are defined

---
+++ src/main/java/com/trendyol/entity/User.java
@@ -0,0 +1,35 @@
+package com.trendyol.entity;
+
+public class User {
+
+    private long id;
+    private String name;
+    private String role;
+
+    protected  User(){
+
+    }
+
+    public User(String name, String role) {
+        super();
+        this.name = name;
+        this.role = role;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("User [id=%s, name=%s, role=%s]", id, name, role);
+    }
+}
