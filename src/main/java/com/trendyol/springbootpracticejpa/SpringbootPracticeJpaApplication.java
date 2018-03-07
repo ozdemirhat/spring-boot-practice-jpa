18977cff90df47202d71b05350066f1b0c794fc4
hatice.ozdemir <hatice.ozdemir@trendyol.com>
2018-03-07 17:02:02 +0300

    initial commit

---
+++ src/main/java/com/trendyol/springbootpracticejpa/SpringbootPracticeJpaApplication.java
@@ -0,0 +1,12 @@
+package com.trendyol.springbootpracticejpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootPracticeJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootPracticeJpaApplication.class, args);
+	}
+}
---
+++ src/test/java/com/trendyol/springbootpracticejpa/SpringbootPracticeJpaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.trendyol.springbootpracticejpa;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootPracticeJpaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
