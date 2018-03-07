0e3776ee19445b58f10329792a388406ff1085b8
hatice.ozdemir <hatice.ozdemir@trendyol.com>
2018-03-07 17:15:08 +0300

    package name is fixed

--- src/main/java/com/trendyol/Application.java
+++ src/main/java/com/trendyol/Application.java
@@ -1,4 +1,4 @@
-package com.trendyol.springbootpracticejpa;
+package com.trendyol;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
